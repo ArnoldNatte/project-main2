d7c7173b06f9fff98017f13fd33618f0e7908970
ArnoldNatte <arnold.natte@gmail.com>
2023-09-06 17:12:47 -0500

    cambios 2

--- projectMain.js
+++ projectMain.js
@@ -1,2 +1,3 @@
 console.log("this is a Project Main");
-console.log("why this archive not figure in the remote repository?")
+console.log("why this archive not figure in the remote repository?");
+console.log("this edit you will se in the project-main archive");
